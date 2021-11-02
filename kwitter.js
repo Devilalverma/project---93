e69a6e958ed5450c6cb638b3c464226727fdd621
Diavyansh <86036331+Devilalverma@users.noreply.github.com>
2021-11-02 17:55:20 +0530

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,15 @@
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAJf26_8lGLW_eGd84eL5EEoGSo4gFyg8A",
+    authDomain: "ensteinbot-tuho.firebaseapp.com",
+    databaseURL: "https://ensteinbot-tuho-default-rtdb.firebaseio.com",
+    projectId: "ensteinbot-tuho",
+    storageBucket: "ensteinbot-tuho.appspot.com",
+    messagingSenderId: "938799392350",
+    appId: "1:938799392350:web:d34cadf563c6ada4e9f6cf",
+    measurementId: "G-WYQ1C3YY6V"
+  };
+  function adduser(){
+      window.location = "kwitter_room.html";
+  }
